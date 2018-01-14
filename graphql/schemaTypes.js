df057a6be5535c018a3ff8e8e75d6ed8ac9a25cc
Andy Hubert <andy.hubert+8@gmail.com>
2018-01-14 16:44:58 +0200

    Fixed comment examples for TagSet:tags

--- graphql/schemaTypes.js
+++ graphql/schemaTypes.js
@@ -2,7 +2,8 @@
 // Verse:usfm - every word MUST be in a /w enclosure
 // TagSet:id = [2-digit book][3-digit chapter][3-digit verse]-[versionCode] (eg. 01001001-esv)
 // TagSet:tags = an array of the original language word numbers (including chapter and verse, if different; including word part number, if multi-part) and translation word numbers
-  // (eg. Gen 1:1's for the esv would be [{o:["||1"],t:[1]},{o:["||2"],t:[3]},...] wherein "בראשית" is tagged to "In" and "beginning")
+  // (eg. Gen 1:1's for the esv would be [{o:["|1|1"],t:[1]},{o:["|1|2"],t:[3]},...] wherein "בראשית" is tagged to "In" and "beginning")
+  // (eg. In an instance where the chapter and verse is different: [{o:["4:1|3|1"],t:[4]},{o:["4:1|3|2"],t:[5,6]},...] wherein 4:1, 3rd word, 1st part tags to 4th word in the translation, and 4:1, 3rd word, 2nd part tags to 5th word and 6th words in the translation)
 // Definition:id = [extended strongs number]-[language] (eg. H234a-eng, G8289-esp; language is needed for the gloss)
 // Definition:pos = an array of parts of speech abbreviations (N, V, etc) that this lemma is found in
 // Definition:syn and Definition:rel = arrays of synonym/related word objects (eg. [{"lemma":"τέκνον","strongs":"G5043","hits":99,"gloss":"child"},...])
