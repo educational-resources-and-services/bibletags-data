d9b00ecd7aa3e70f331260ffcfab692373456db1
Huberts <huberts@Hubertss-MBP.lan>
2019-02-06 21:32:39 +0200

    Updated definitionId regex

--- graphql/utils.js
+++ graphql/utils.js
@@ -9,7 +9,7 @@ module.exports = {
   wordIdRegEx: /^[0-9]{2}[0-9a-z]{3}$/i,
   locRegEx: /^[0-9]{8}$/,
   versionIdRegEx: /^[a-z0-9]{2,9}$/,
-  definitionIdRegEx: /^[HAG][0-9]{5}[a-z]?$/,  // strongs number
+  definitionIdRegEx: /^[HAG][0-9]{1,4}(?:\.[0-9])?$/,  // strongs number
   languageIdRegEx: /^[a-z]{3}$/,
   scopeRegEx: /^[a-z]{1,2}|[0-9]{2}$/,
   wordsHashRegEx: /^[a-zA-Z0-9\+\/=]{2,}$/,  // base 64 digits only
