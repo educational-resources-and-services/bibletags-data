b417680f3caffde56dc17fd92cfbb97178fdc6bd
AndyHubert <andy.hubert@gmail.com>
2022-03-15 17:09:32 -0400

    Fixed do-setup-db to work with auth

--- src/scripts/deleteUsersAndSessionsTables.js
+++ src/scripts/deleteUsersAndSessionsTables.js
@@ -12,9 +12,12 @@ const mysql = require('mysql2/promise')
     multipleStatements: true,
   })
 
-  console.log(`\nSTARTING deleteSessionsTable...\n`)
+  console.log(`\nSTARTING deleteUsersAndSessionsTables...\n`)
 
-  await connection.query(`DELETE TABLE sessions`)
+  await connection.query(`SET foreign_key_checks = 0`)
+  await connection.query(`DROP TABLE IF EXISTS sessions`)
+  await connection.query(`DROP TABLE IF EXISTS users`)
+  await connection.query(`SET foreign_key_checks = 1`)
 
   console.log(`\nCOMPLETED\n`)
   process.exit()
