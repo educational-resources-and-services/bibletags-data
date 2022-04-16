930da8b361fd70ed6faff271f64d4e1939632fe7
AndyHubert <andy.hubert@gmail.com>
2022-04-16 10:03:44 -0400

    Run cron hourly for now

--- handler.js
+++ handler.js
@@ -5,9 +5,13 @@ const handler = async ({ forceRunAll }={}) => {
   const day = new Date().getDay()  // 0-6
   const hours = new Date().getHours()  // 0-23
   const minutes = new Date().getMinutes()  // 0-59
-  
-  if((hours === 0 && minutes === 0) || forceRunAll) {  // once per day
+
+  if((minutes === 0) || forceRunAll) {  // once per hour
     await updateWordTranslationsAndLanguageSpecificDefinitions()
+  }
+
+  if((hours === 0 && minutes === 0) || forceRunAll) {  // once per day
+    // await updateWordTranslationsAndLanguageSpecificDefinitions()
   }
 
 }
