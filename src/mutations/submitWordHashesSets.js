83b559ce7abef8564d066f9ffab8f8a5f3353b62
AndyHubert <andy.hubert@gmail.com>
2022-05-13 13:26:51 -0400

    prevent excessive db connections in submitWordHashesSets

--- src/mutations/submitWordHashesSets.js
+++ src/mutations/submitWordHashesSets.js
@@ -1,9 +1,20 @@
 const submitWordHashesSet = require('./submitWordHashesSet')
 
+const NUMBER_AT_A_TIME = 3
+
 const submitWordHashesSets = async (args, req, queryInfo) => {
 
   const { input } = args
-  await Promise.all(input.map(set => submitWordHashesSet({ input: set, skipGetTagSet: true })))
+
+  let idx = -1
+
+  const submitOne = async () => {
+    if(++idx >= input.length) return
+    await submitWordHashesSet({ input: input[idx], skipGetTagSet: true })
+    await submitOne()
+  }
+
+  await Promise.all(Array(NUMBER_AT_A_TIME).fill().map(submitOne))
 
   return true
 }
